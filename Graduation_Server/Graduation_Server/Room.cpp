284368051da5a658fc3aeaf6b578911562705b74
OnlyFall <kwb010712@naver.com>
2022-12-15 01:28:27 +0900

    RoomManager 만드는중!

---
+++ Graduation_Server/Graduation_Server/Room.cpp
@@ -0,0 +1,11 @@
+#include "Room.h"
+
+Room::Room()
+{
+
+}
+
+Room::~Room()
+{
+
+}
--- Graduation_Server/Graduation_Server/Room_Manager.h
+++ Graduation_Server/Graduation_Server/Room_Manager.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "stdafx.h"
+
+class RoomManager
+{
+private:
+	array<int, 6> in_game_id;
+
+public:
+	RoomManager();
+	~RoomManager();
+};
