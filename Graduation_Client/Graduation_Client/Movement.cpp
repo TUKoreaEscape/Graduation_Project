5e19b9110bd546489de6537462ba98590a85871d
박동규 <ssudomt@naver.com>
2023-02-16 22:03:53 +0900

    bug fix
    한글을 사용하면 글자가 깨지고 빌드가 안되던 버그

--- Graduation_Client/Graduation_Client/Movement.cpp
+++ Graduation_Client/Graduation_Client/Movement.cpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "Movement.h"
 #include "Input.h"
 
@@ -11,11 +11,11 @@ void CommonMovement::update()
 	UCHAR keyBuffer[256];
 	memcpy(keyBuffer,Input::GetInstance()->keyBuffer, (sizeof(keyBuffer)));
 	if (keyBuffer['w'] & 0xF0 || keyBuffer['W'] & 0xF0)
-		std::cout << "wŰ" << std::endl;
+		std::cout << "w키" << std::endl;
 	if (keyBuffer['s'] & 0xF0 || keyBuffer['S'] & 0xF0)
-		std::cout << "sŰ" << std::endl;
-	if (keyBuffer['a'] & 0xF0 || keyBuffer['A'] & 0xF0)std::cout << "aŰ" << std::endl;
-	if (keyBuffer['d'] & 0xF0 || keyBuffer['D'] & 0xF0) std::cout << "dŰ" << std::endl;
-	if (keyBuffer['q'] & 0xF0 || keyBuffer['Q'] & 0xF0) std::cout << "qŰ" << std::endl;
-	if (keyBuffer['e'] & 0xF0 || keyBuffer['E'] & 0xF0)std::cout << "eŰ" << std::endl;
+		std::cout << "s키" << std::endl;
+	if (keyBuffer['a'] & 0xF0 || keyBuffer['A'] & 0xF0)
+		std::cout << "a키" << std::endl;
+	if (keyBuffer['d'] & 0xF0 || keyBuffer['D'] & 0xF0) 
+		std::cout << "d키" << std::endl;
 }
