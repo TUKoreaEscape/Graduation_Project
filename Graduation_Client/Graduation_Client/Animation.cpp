bb8a0be43a23a0d855683688bfd86bb47712930f
박동규 <ssudomt@naver.com>
2023-03-01 16:16:04 +0900

    file_upload

---
+++ Graduation_Client/Graduation_Client/Animation.cpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "Animation.h"
--- Graduation_Client/Graduation_Client/stdafx.h
+++ Graduation_Client/Graduation_Client/stdafx.h
@@ -42,3 +42,9 @@ using Microsoft::WRL::ComPtr;
 #include <deque>
 #include <algorithm>
 #include <iostream>
+
+
+extern ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, void* pData, UINT nBytes,
+	D3D12_HEAP_TYPE d3dHeapType = D3D12_HEAP_TYPE_UPLOAD,
+	D3D12_RESOURCE_STATES d3dResourceStates = D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER,
+	ID3D12Resource** ppd3dUploadBuffer = NULL);
