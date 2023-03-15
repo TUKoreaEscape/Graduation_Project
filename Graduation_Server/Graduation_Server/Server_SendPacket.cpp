6103e9e62f51813cc047a652f6f2213a492135d0
OnlyFall <kwb010712@naver.com>
2023-03-15 14:53:35 +0900

    packet, session 파일 분리

---
+++ Graduation_Server/Graduation_Server/Server_SendPacket.cpp
@@ -0,0 +1,105 @@
+#include "GameServer.h"
+
+void cGameServer::send_chat_packet(const unsigned int user_id, const unsigned int my_id, char* mess)
+{
+	sc_packet_chat packet;
+	//packet.id = my_id;
+	m_clients[my_id].get_client_name(*packet.name, MAX_NAME_SIZE);
+	packet.size = sizeof(packet);
+	packet.type = SC_PACKET::SC_PACKET_CHAT;
+	strcpy_s(packet.message, mess);
+
+	m_clients[user_id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_login_fail_packet(const unsigned int user_id, LOGIN_FAIL_REASON::TYPE reason)
+{
+	sc_packet_login_fail packet;
+	packet.type = SC_PACKET::SC_LOGINFAIL;
+	packet.size = sizeof(sc_packet_login_fail);
+	packet.reason = reason;
+	m_clients[user_id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_login_ok_packet(const unsigned int user_id)
+{
+	sc_packet_login_ok packet;
+	packet.id = user_id;
+	packet.size = sizeof(sc_packet_login_ok);
+	packet.type = SC_PACKET::SC_LOGINOK;
+
+	m_clients[user_id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_create_id_ok_packet(const unsigned int user_id)
+{
+	sc_packet_create_id_ok packet;
+	packet.size = sizeof(packet);
+	packet.type = SC_PACKET::SC_CREATE_ID_OK;
+
+	m_clients[user_id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_create_id_fail_packet(const unsigned int user_id, char reason)
+{
+	sc_packet_create_id_fail packet;
+	packet.size = sizeof(packet);
+	packet.type = SC_PACKET::SC_CREATE_ID_FAIL;
+	packet.reason = reason;
+
+	m_clients[user_id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_create_room_ok_packet(const unsigned int user_id, const int room_number)
+{
+	sc_packet_create_room packet;
+	packet.size = sizeof(packet);
+	packet.type = SC_PACKET::SC_CREATE_ROOM_OK;
+	packet.room_number = room_number;
+
+	m_clients[user_id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_join_room_success_packet(const unsigned int user_id)
+{
+	sc_packet_join_room_success packet;
+	packet.size = sizeof(packet);
+	packet.type = SC_PACKET::SC_PACKET_JOIN_ROOM_SUCCESS;
+
+	packet.room_number = m_clients[user_id].get_join_room_number();
+
+	m_clients[user_id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_join_room_fail_packet(const unsigned int user_id)
+{
+	sc_packet_join_room_fail packet;
+	packet.size = sizeof(packet);
+	packet.type = SC_PACKET::SC_PACKET_JOIN_ROOM_FAIL;
+
+	m_clients[user_id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_game_start_packet(const unsigned int id)
+{
+	sc_packet_game_start packet;
+	packet.size = sizeof(packet);
+	packet.type = SC_PACKET::SC_PACKET_GAME_START;
+
+	m_clients[id].do_send(sizeof(packet), &packet);
+}
+
+void cGameServer::send_move_packet(const unsigned int id, const unsigned int moved_id, XMFLOAT3 pos)
+{
+	sc_update_user_packet packet;
+
+	packet.size = sizeof(packet);
+	packet.type = SC_PACKET::SC_USER_UPDATE;
+
+	packet.data.id = moved_id;
+	packet.data.position = m_clients[moved_id].get_user_position();
+	packet.data.velocity = m_clients[moved_id].get_user_velocity();
+	packet.data.yaw = m_clients[moved_id].get_user_yaw();
+	packet.data.active;
+	m_clients[id].do_send(sizeof(packet), &packet);
+}
---
+++ Graduation_Server/Graduation_Server/Update_Session.cpp
@@ -0,0 +1,47 @@
+#include "GameServer.h"
+
+
+void cGameServer::Update_Session()
+{
+	while (true)
+	{
+		if (m_PerformanceCounter.Frame_Limit(1.f)) // 초당 1번 업데이트!
+		{
+			//cout << "Update Session!" << endl;
+			for (int i = 0; i < MAX_ROOM; ++i)
+			{
+				Room& rl = *m_room_manager->Get_Room_Info(i);
+				rl._room_state_lock.lock();
+				switch (rl._room_state)
+				{
+				case GAME_ROOM_STATE::FREE:
+				{
+					rl._room_state_lock.unlock();
+					break;
+				}
+
+				case GAME_ROOM_STATE::READY:
+				{
+					rl._room_state_lock.unlock();
+					break;
+				}
+
+				case GAME_ROOM_STATE::PLAYING:
+				{
+					rl._room_state_lock.unlock();
+					rl.Update_room_time();
+					break;
+				}
+
+				case GAME_ROOM_STATE::END:
+				{
+					rl._room_state_lock.unlock();
+					break;
+				}
+
+				}
+
+			}
+		}
+	}
+}
