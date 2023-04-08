5bc0d8397b0f091649f11df29d1b58a14a87b20e
kimwoobin <kwb010712@naver.com>
2023-04-08 23:24:17 +0900

    Update Network.h

--- Graduation_Client/Graduation_Client/Network.h
+++ Graduation_Client/Graduation_Client/Network.h
@@ -12,7 +12,7 @@ class Network {
 private:
 	static Network* NetworkInstance;
 	SOCKET			m_socket;
-	const char*		SERVER_ADDR = "183.101.112.30";
+	const char*		SERVER_ADDR = "127.0.0.1";
 	Server_Timer	m_server_counter;
 public:
 	std::mutex pos_lock;
