55b9f4b70c34a0772575f984b056bb65404f1520
Hwang Seok Ju <asa4163@gmail.com>
2023-07-16 23:56:31 +0900

    Revert "단면도 찍는 커밋 // 취소할예정"
    This reverts commit 9695452f39c9de6331a6ccf75adf8b5479e04d15.

--- Graduation_Client/Graduation_Client/GameScene.cpp
+++ Graduation_Client/Graduation_Client/GameScene.cpp
@@ -55,9 +55,9 @@ void GameScene::prerender(ID3D12GraphicsCommandList* pd3dCommandList)
 
 void GameScene::defrender(ID3D12GraphicsCommandList* pd3dCommandList)
 {
-	//m_pSkybox->render(pd3dCommandList);
+	m_pSkybox->render(pd3dCommandList);
 
-	//m_pCeilling->render(pd3dCommandList);
+	m_pCeilling->render(pd3dCommandList);
 
 	m_pMainTerrain->render(pd3dCommandList);
 	m_pPianoTerrain->render(pd3dCommandList);
@@ -70,7 +70,7 @@ void GameScene::defrender(ID3D12GraphicsCommandList* pd3dCommandList)
 	{
 		if (m_ppWalls[i]) m_ppWalls[i]->render(pd3dCommandList);
 	}
-	m_pPlayer->SetPosition(XMFLOAT3(20.0f,110.0f, 0.0f));
+
 	Scene::render(pd3dCommandList);
 
 	for (auto p : m_sPVS[static_cast<int>(m_pvsCamera)]) {
@@ -163,7 +163,6 @@ void GameScene::UIrender(ID3D12GraphicsCommandList* pd3dCommandList)
 		}
 		break;
 	case PLAYING_GAME:
-		break;
 		for (int i = 0; i < NUM_DOOR; ++i) {
 			reinterpret_cast<Door*>(m_pDoors[i])->UIrender(pd3dCommandList);
 		}
@@ -1369,7 +1368,7 @@ void GameScene::BuildObjectsThread(ID3D12Device* pd3dDevice, ID3D12GraphicsComma
 	m_pPlayer->AddComponent<CommonMovement>();
 
 	for (int i = 0; i < m_nPlayers; ++i) {
-		//AddPlayer(m_ppPlayers[i]);
+		AddPlayer(m_ppPlayers[i]);
 	}
 	AddPlayer(m_pPlayer);
 
