7b5ba62068e477cec4f3a3f1c888e0b8f00b8d8a
OnlyFall <kwb010712@naver.com>
2023-07-20 00:12:55 +0900

    <조건분기 생성>

--- Graduation_Server/Graduation_Server/GameServer.cpp
+++ Graduation_Server/Graduation_Server/GameServer.cpp
@@ -804,40 +804,28 @@ void cGameServer::ProcessPacket(const unsigned int user_id, unsigned char* p) //
 
 	case CS_PACKET::CS_PACKET_CREATE_ROOM:
 	{
-#if !DEBUG
 		if (Y_LOGIN == m_clients[user_id].get_login_state() && m_clients[user_id].get_state() == CLIENT_STATE::ST_LOBBY) // 로그인하고 로비에 있을때만 방 생성 가능
-#endif
 			Process_Create_Room(user_id, p);
 		break;
 	}
 
 	case CS_PACKET::CS_PACKET_JOIN_ROOM:
 	{
-#if !DEBUG
 		if (Y_LOGIN == m_clients[user_id].get_login_state() && m_clients[user_id].get_state() == CLIENT_STATE::ST_LOBBY)
-#endif
-#if !DEBUG
-			if (m_clients[user_id].get_state() == CLIENT_STATE::ST_LOBBY)
-#endif
-		//cout << "recv Join Room" << endl;
-		Process_Join_Room(user_id, p);
+			Process_Join_Room(user_id, p);
 		break;
 	}
 
 	case CS_PACKET::CS_PACKET_READY:
 	{
-#if !DEBUG
 		if (Y_LOGIN == m_clients[user_id].get_login_state() && m_clients[user_id].get_state() == CLIENT_STATE::ST_GAMEROOM)
-#endif
 			Process_Ready(user_id, p);
 		break;
 	}
 	
 	case CS_PACKET::CS_PACKET_EXIT_ROOM:
 	{
-#if !DEBUG
 		if (Y_LOGIN == m_clients[user_id].get_login_state() && m_clients[user_id].get_state() == CLIENT_STATE::ST_GAMEROOM)
-#endif
 			Process_Exit_Room(user_id, p);
 		break;
 	}
