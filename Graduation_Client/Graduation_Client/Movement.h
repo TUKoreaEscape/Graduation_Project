9a473d74dd621bd462fa1bbdbfcc190c9816b01b
박동규 <ssudomt@naver.com>
2023-11-07 06:59:43 +0900

    오류수정

--- Graduation_Client/Graduation_Client/Movement.h
+++ Graduation_Client/Graduation_Client/Movement.h
@@ -11,20 +11,20 @@ public:
 class TaggerMovement : public Component
 {
 public:
-	void start(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList) {};
-	void update(float elapsedTime) {};
+	void start(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	void update(float elapsedTime);
 };
 
 class SurvivorMovement : public Component
 {
 public:
-	void start(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList) {};
-	void update(float elapsedTime) {};
+	void start(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	void update(float elapsedTime);
 };
 
 class DeadMovement : public Component
 {
 public:
-	void start(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList) {};
-	void update(float elapsedTime) {};
+	void start(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+	void update(float elapsedTime);
 };
